a75dce3f0e244900cc9fb128bbf0d1f4f5b4c0c5
Min Zhang <belem@163.com>
2024-06-26 17:10:18 +0800

    init

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,13 +2,6 @@
 import { redirect } from '@sveltejs/kit'
 import type { PageServerLoad } from './$types'
 
-export const load: PageServerLoad = async ({ url, locals: { safeGetSession } }) => {
-  const { session } = await safeGetSession()
-
-  // if the user is already logged in return them to the account page
-  if (session) {
-    redirect(303, '/profile')
-  }
-
+export const load: PageServerLoad = async ({ url }) => {
   return { url: url.origin }
 }
